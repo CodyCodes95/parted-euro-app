c145c14bdffa22dd3f8f41ff619025914f59f52c
Cody Partington <codythatsme@gmail.com>
2025-03-11 22:04:58 +1100

    refactor

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,17 +1,5 @@
-import Link from "next/link";
-import { auth } from "~/server/auth";
 import { HydrateClient } from "~/trpc/server";
 
-export default async function Home() {
-  const session = await auth();
-
-  // if (session?.user) {
-  //   void api.post.getLatest.prefetch();
-  // }
-
-  return (
-    <HydrateClient>
-      <main>admin</main>
-    </HydrateClient>
-  );
+export default async function Page() {
+  return <p>admin</p>;
 }
