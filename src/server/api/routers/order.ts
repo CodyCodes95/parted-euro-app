c0ad2c945f947a82af431a6116985a590781df7e
Cody Partington <codythatsme@gmail.com>
2025-04-04 23:10:18 +1100

    get order method updated

--- src/server/api/routers/order.ts
+++ src/server/api/routers/order.ts
@@ -6,7 +6,12 @@ export const orderRouter = createTRPCRouter({
   getOrderById: publicProcedure
     .input(z.string())
     .query(async ({ ctx, input }) => {
-      const order = await ctx.db.order.findUnique({ where: { id: input } });
+      const order = await ctx.db.order.findUnique({
+        where: { id: input },
+        include: {
+          orderItems: true,
+        },
+      });
       return order;
     }),
 });
