e2caaee6f648aa359d79b315687a22e3993f3815
Cody Partington <codythatsme@gmail.com>
2025-04-29 02:14:01 +1000

    mode auto

--- src/app/admin/donors/_components/donor-form.tsx
+++ src/app/admin/donors/_components/donor-form.tsx
@@ -557,6 +557,7 @@ export function DonorForm({
                 {/* Image Upload Dropzone */}
                 <div className="mb-4">
                   <UploadDropzone
+                    config={{mode: "au"}}
                     endpoint="homepageImage"
                     onBeforeUploadBegin={(files) => {
                       // Create a promise for each file to be compressed
