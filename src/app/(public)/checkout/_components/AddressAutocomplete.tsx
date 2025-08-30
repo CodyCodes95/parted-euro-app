18d18b1e4648b6bf6f15a8bf7dcb69cafb12cd37
Cody Partington <codythatsme@gmail.com>
2025-08-30 21:40:53 +1000

    only filtering postcode for now

--- src/app/(public)/checkout/_components/AddressAutocomplete.tsx
+++ src/app/(public)/checkout/_components/AddressAutocomplete.tsx
@@ -190,12 +190,12 @@ function AddressAutoCompleteInput(props: CommonProps) {
                       .filter((prediction) =>
                         prediction.types.includes("postal_code"),
                       )
-                      .filter((prediction) =>
-                        prediction.types.includes("locality"),
-                      )
-                      .filter((prediction) =>
-                        prediction.types.includes("administrative_area_level_1"),
-                      )
+                      // .filter((prediction) =>
+                      //   prediction.types.includes("locality"),
+                      // )
+                      // .filter((prediction) =>
+                      //   prediction.types.includes("administrative_area_level_1"),
+                      // )
                       .map((prediction) => (
                         <CommandItem
                           value={prediction.place_id}
